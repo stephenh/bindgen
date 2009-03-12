e7c9548b35b43bbac3ef8489917d5896871af15c
Stephen Haberman <stephen@exigencecorp.com>
2009-03-12 12:11:58 -0500

    Generate BindKeywords in the first empty round instead of the last round. Eclipse seems to prefer this.

--- bindgen/src/org/exigencecorp/bindgen/processor/BindgenAnnotationProcessor.java
+++ bindgen/src/org/exigencecorp/bindgen/processor/BindgenAnnotationProcessor.java
@@ -20,15 +20,18 @@ import org.exigencecorp.bindgen.Bindable;
 public class BindgenAnnotationProcessor extends AbstractProcessor {
 
     private GenerationQueue queue;
+    private boolean hasUpdatedKeywordClass = false;
 
     @Override
     public void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
         this.queue = new GenerationQueue(processingEnv);
+        this.hasUpdatedKeywordClass = false;
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        boolean emptyRound = true;
         for (Element element : roundEnv.getElementsAnnotatedWith(Bindable.class)) {
             if (element instanceof PackageElement) {
                 for (Element nested : ((PackageElement) element).getEnclosedElements()) {
@@ -39,10 +42,15 @@ public class BindgenAnnotationProcessor extends AbstractProcessor {
             } else {
                 this.processingEnv.getMessager().printMessage(Kind.WARNING, "Unhandled element " + element);
             }
+            emptyRound = false;
         }
         this.queue.processQueue();
-        if (roundEnv.processingOver()) {
+        if (emptyRound && !this.hasUpdatedKeywordClass) {
+            // Updating the Keyword class on the official processingOver() round did not
+            // work in Eclipse, but we seem to get an "empty" round before the "over"
+            // round, so detect that and update the keyword class there.
             this.queue.updateBindKeywordClass();
+            this.hasUpdatedKeywordClass = true;
         }
         return true;
     }
