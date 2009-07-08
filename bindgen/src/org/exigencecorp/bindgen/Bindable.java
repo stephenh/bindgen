2dd23e64db05259d45b0d9177e827a7a3036846a
Stephen Haberman <stephen@exigencecorp.com>
2009-07-08 10:59:18 -0500

    Add @Inherited.

--- bindgen/src/org/exigencecorp/bindgen/Bindable.java
+++ bindgen/src/org/exigencecorp/bindgen/Bindable.java
@@ -1,8 +1,10 @@
 package org.exigencecorp.bindgen;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Target;
 
+@Inherited
 @Target(value = { ElementType.TYPE })
 public @interface Bindable {
 
