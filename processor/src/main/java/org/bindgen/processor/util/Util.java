78a7a190f1add622e6cce3e27aa6eeac8c7c21ec
Stephen Haberman <stephen@exigencecorp.com>
2010-06-30 01:14:36 -0500

    Add missing Object method names.

--- processor/src/main/java/org/bindgen/processor/util/Util.java
+++ processor/src/main/java/org/bindgen/processor/util/Util.java
@@ -26,7 +26,7 @@ public class Util {
 	private static final Pattern lowerCase = Pattern.compile("^[a-z]");
 	private static final String[] javaKeywords = "abstract,continue,for,new,switch,assert,default,goto,package,synchronized,boolean,do,if,private,this,break,double,implements,protected,throw,byte,else,import,public,throws,case,enum,instanceof,return,transient,catch,extends,int,short,try,char,final,interface,static,void,class,finally,long,strictfp,volatile,const,float,native,super,while,null"
 		.split(",");
-	private static final String[] objectMethodNames = { "hashCode", "toString", "clone" };
+	private static final String[] objectMethodNames = { "hashCode", "toString", "clone", "notify", "notifyAll", "wait", "finalize" };
 	private static final String[] bindingMethodNames = { "get", "getPath", "getType", "getParentBinding", "getChildBindings", "getIsSafe", "getSafely" };
 
 	// Watch for package.Foo.Inner -> package.foo.Inner
